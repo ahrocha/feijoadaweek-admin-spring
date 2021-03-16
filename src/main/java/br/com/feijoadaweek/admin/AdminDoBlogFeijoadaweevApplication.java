615ad66229390e85865ded7245efb876d6b8e857
Andrey <andrey.rocha@pravaler.com.br>
2021-03-16 19:32:48 -0300

    Commit inicial

---
+++ src/main/java/br/com/feijoadaweek/admin/AdminDoBlogFeijoadaweevApplication.java
@@ -0,0 +1,13 @@
+package br.com.feijoadaweek.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDoBlogFeijoadaweevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDoBlogFeijoadaweevApplication.class, args);
+	}
+
+}
