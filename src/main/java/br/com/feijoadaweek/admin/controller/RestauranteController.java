6f9abb9384116dd163657af4824190b790e2d81b
Andrey <andrey.rocha@pravaler.com.br>
2021-03-31 19:24:38 -0300

    Gravação de novo restaurante

--- src/main/java/br/com/feijoadaweek/admin/controller/RestauranteController.java
+++ src/main/java/br/com/feijoadaweek/admin/controller/RestauranteController.java
@@ -2,7 +2,6 @@ package br.com.feijoadaweek.admin.controller;
 
 import java.util.List;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -18,10 +17,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.feijoadaweek.admin.dto.RequisicaoNovoPrato;
+import br.com.feijoadaweek.admin.dto.RequisicaoNovoRestaurante;
 import br.com.feijoadaweek.admin.model.Prato;
 import br.com.feijoadaweek.admin.model.Restaurante;
 import br.com.feijoadaweek.admin.repository.PratoRepository;
@@ -42,20 +44,44 @@ public class RestauranteController {
 	@Autowired
 	private RestauranteRepository restauranteRepository;
 
-	@RequestMapping("/restaurantes")
-	public String hello(Model model) {
+	@GetMapping("/restaurantes")
+	public String list(Model model) {
 
-		System.out.println("restaurantes");
+		System.out.println("listando restaurantes");
 
-		List<Prato> pratos = pratoRepository.findTop10ByOrderByDataDesc();
+//		List<Prato> pratos = pratoRepository.findTop10ByOrderByDataDesc();
+		List<Restaurante> restaurantes = restauranteRepository.findTop100ByOrderByDataDesc();
 
-		model.addAttribute("pratos", pratos);
+		model.addAttribute("restaurantes", restaurantes);
 
 		return "restaurantes";
 	}
 
+	@PostMapping("/restaurantes")
+	public String create(Model model, @Valid RequisicaoNovoRestaurante requisicao, BindingResult result) {
+
+		System.out.println("criando restaurante");
+		
+		if (result.hasErrors()) {
+			for (ObjectError error : result.getAllErrors()) {
+				System.out.println(error.getDefaultMessage());
+			}
+			System.out.println("tem erro");
+			return this.list(model);
+		}
+
+		Restaurante restaurante = requisicao.toRestaurante();
+		System.out.println("adicionando restaurante " + restaurante.getNome());
+		System.out.println("adicionando slug do restaurante " + restaurante.getSlug());
+
+		// @TODO validar slug repetido
+		restauranteRepository.save(restaurante);
+
+		return this.list(model);
+	}
+
 	@RequestMapping("/restaurante/{slug}")
-	public String hello(@PathVariable("slug") String slug, @Valid RequisicaoNovoPrato requisicao, BindingResult result,
+	public String show(@PathVariable("slug") String slug, @Valid RequisicaoNovoPrato requisicao, BindingResult result,
 			Model model) {
 
 		List<Restaurante> restaurantes = restauranteRepository.findBySlug(slug);
