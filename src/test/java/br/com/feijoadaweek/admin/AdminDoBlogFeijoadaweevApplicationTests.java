df4dc682dbef479a26b896ab33b8e18b77dfed90
Andrey <andrey.rocha@pravaler.com.br>
2021-05-12 18:51:57 -0300

    Configs for testing fixed

---
+++ src/test/java/br/com/feijoadaweek/admin/AdminDoBlogFeijoadaweevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.feijoadaweek.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminDoBlogFeijoadaweevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
